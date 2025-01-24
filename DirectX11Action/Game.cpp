1139782c3e2f760f33a91b59e9071ae83b52a649
GyuJae <wjdrbwo1206@naver.com>
2025-01-24 15:54:04 +0900

    기본 프레임워크

---
+++ DirectX11Action/Game.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "Game.h"
+
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+void Game::Init(HWND hwnd)
+{
+	this->_hwnd = hwnd;
+	this->_width = GWinSizeX;
+	this->_height = GWinSizeY;
+
+	// TODO
+}
+
+void Game::Update()
+{
+
+}
+
+void Game::Render()
+{
+
+}
